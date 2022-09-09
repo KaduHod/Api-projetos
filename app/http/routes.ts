dff809b4ec601457f022a002dad32e697a03cb7d
Carlos <carlosjr.ribas@gmail.com>
2022-09-08 21:26:35 -0300

    'deploy'

--- app/http/routes.ts
+++ app/http/routes.ts
@@ -1,15 +1,6 @@
 import express from 'express';
 import ProjectController from './controllers/projectController';
-import ConnectionParams from '../interfaces/ConnectionParams';
-import MongoDB from '../database/MongoDB';
-import ProjectRepository from '../repository/ProjectRepository';
-import { Request, Response } from 'express';
-
-
 const Routes = express.Router();
-
 Routes.get('/list', ProjectController.listProjects);
-// Routes.post('/update/{id}', ProjectController.update);
 Routes.get('/:projectId', ProjectController.get);
-
 export default Routes;
